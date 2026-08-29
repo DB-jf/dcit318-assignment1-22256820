7a4d8c5ad5b77721a6366a86348d0868cb9b1f7c
Daniel Badipehter <bdaniel001@st.ug.edu.gh>
2026-08-29 02:09:48 +0000

    Add Grade Calculator application

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your numerical grade (0 - 100): ");
+        int grade = Convert.ToInt32(Console.ReadLine());
+
+        if (grade < 0 || grade > 100)
+        {
+            Console.WriteLine("Invalid grade. Please enter a value between 0 and 100.");
+        }
+        else if (grade >= 90)
+        {
+            Console.WriteLine("Letter Grade: A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Letter Grade: B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Letter Grade: C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Letter Grade: D");
+        }
+        else
+        {
+            Console.WriteLine("Letter Grade: F");
+        }
+    }
+}
